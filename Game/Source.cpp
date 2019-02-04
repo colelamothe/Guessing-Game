e7e321f8af1bbcd9cad7e29376b905f8d9b80eca
cole lamothe <Mothman92@hotmail.com>
2019-02-04 00:12:22 -0500

    Number guessing game

---
+++ Game/Source.cpp
@@ -0,0 +1,40 @@
+//Random number guessing game
+
+#include <iostream>
+#include <time.h> // imports the time function
+
+using namespace std;
+
+int main() {
+	srand(time(NULL)); //initializes the random function with a seed based on the current time
+	
+	int guess; //player guess
+	int guessCount = 1; //game starts with first guess to allow increment to work properly
+	char cont = 'y'; //starts continue value as true
+
+	do { //overall shell to allow the game to be played multiple times on player input
+		int randomNumber = rand() % 10; //generates a random number and bounds it from 0-10 by using the modulo function
+		cout << "This program generates a number from 0-10" << endl;
+		cout << "Make your first guess: " << endl;
+		cin >> guess; // receives first guess
+			while (guess != randomNumber) { //runs the loop until the player guesses correctly
+				if (guess < randomNumber) {
+				cout << "Too low, try again: " << endl; // basic hint and receives new guess input 
+				cin >> guess;
+				}
+				else {
+					cout << "Too high, try again: " << endl; // basic hint and new guess input
+					cin >> guess;
+			}
+			++guessCount; // increments guess count each time through the loop
+
+		}
+		cout << "Correct, the number was " << randomNumber << " and it took you " << guessCount << " attempts to guess it." << endl; // outputs success message and guess count
+		cout << "Would you like to continue? y/n?" << endl;
+		cin >> cont; // once the game has run once prompts for repeat input
+	} while (cont == 'y'); //allows the game to loop if the player inputs the character 'y' and exits otherwise
+	
+
+	system("pause");
+	return 0;
+}
